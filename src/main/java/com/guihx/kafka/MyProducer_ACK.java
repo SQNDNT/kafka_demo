360f238f847ead8a9a7ba269c9eb6f650ad8534f
桂华新 <525901934@qq.com>
2021-11-01 14:34:52 +0800

    ack配置发送失败的重试次数和时间间隔

--- src/main/java/com/guihx/kafka/MyProducer_ACK.java
+++ src/main/java/com/guihx/kafka/MyProducer_ACK.java
@@ -18,6 +18,20 @@ public class MyProducer_ACK {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        /**
+         * 配置ack，仅同步下有效
+         * 0:消息发送后不用等broker返回ack，高性能低安全，易发生消息丢失
+         * 1:默认配置，消息发送后需要leader将消息写到本地log中后再返回ack,是个均衡模式
+         * -1/all:消息发送后leader接收并同步给follow后返回ack，低性能高安全。须配置min.insync.replicas=2（默认是1，最好配置>=2，否则效果跟ack=1一样）
+         * @Author guihx
+         * @Date 2021-11-1 14:26
+         */
+        props.put(ProducerConfig.ACKS_CONFIG, 1);
+        //发送消息失败后重试3次
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        //重发送间隔(300毫秒)
+        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 300);
+
         Producer<String, String> producer = new KafkaProducer<>(props);
 
         ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(TOPIC_NAME, JSON.toJSONString("同步消息"));
