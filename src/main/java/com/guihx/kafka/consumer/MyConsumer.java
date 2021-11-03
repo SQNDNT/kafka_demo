b4803892bf330124734c9d6e424acbf3d7e4d5fd
桂华新 <525901934@qq.com>
2021-11-03 09:59:50 +0800

    各种配置

--- src/main/java/com/guihx/kafka/consumer/MyConsumer.java
+++ src/main/java/com/guihx/kafka/consumer/MyConsumer.java
@@ -1,4 +1,60 @@
 package com.guihx.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class MyConsumer {
+    private final static String TOPIC_NAME = "test";
+    private final static String GROUP_NAME = "testGroup";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_NAME);
+        //键值对反序列化
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        //是否自动提交offset（默认true）
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"true");
+        //自动提交时间间隔
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
+        /**
+         * earliest 没有消费记录就从头开始，然后记录偏移量，下次从这里开始
+         * latest 拉最新的，就是消费者启动后的消息，前面的不消费
+         * @Author guihx
+         * @Date 2021-11-3 9:56
+         */
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //⼀次poll最⼤拉取消息的条数，可以根据消费速度的快慢来设置
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 500);
+        //如果两次poll的时间如果超出了30s的时间间隔，kafka会认为其消费能⼒过弱，将其踢出消费组。将分区分配给其他消费者。-rebalance
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 30 * 1000);
+
+        //consumer给broker发送⼼跳的间隔时间
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 1000);
+        //kafka如果超过10秒没有收到消费者的⼼跳，则会把消费者踢出消费组，进⾏rebalance，把分区分配给其他消费者。
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 10 * 1000);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("收到消息: topic-" + record.topic() + " | Partition-" + record.partition()
+                        + " | offset-" + record.offset() + " | key-" + record.key() + " | value" + record.value());
+            }
+        }
+
+    }
 }
